2f906a6d2456a3c697047293f5e83a299b14541c
jinyeong.jun <poscoun@gmail.com>
2025-04-17 15:23:14 +0900

    [2024-04-17 전진영] Array list

--- test.py
+++
@@ -1,9 +0,0 @@
-def twoSum(nums, target):
-    n = len(nums)
-    for i in range(n):
-        for j in range(i+1, n):
-            if nums[i] + nums[j] == target:
-                return True
-    return False
-
-print(twoSum(nums=[2,1,5,7], target=14))
